d67a41b20b32b9d2296fc62dd1d1799dd0237147
Alex123Palacin <152417813+Alex123Palacin@users.noreply.github.com>
2023-12-12 12:41:51 -0500

    Update dusuario.java

--- analisis y diseño proyecto final/analisis y diseño proyecto final/Matricula_Asistencia/src/Modelo/dusuario.java
+++ analisis y diseño proyecto final/analisis y diseño proyecto final/Matricula_Asistencia/src/Modelo/dusuario.java
@@ -1,27 +1,18 @@
-
+//Uusario
 package Modelo;
-
 
 public class dusuario {
     
-
 private int idusuario;
 private  String usuario;
 private  String contraseña;
-
 private String cargos;
-
 private String personal;
 private  String direccion;
-
 private String telefono;
-
-
 private String estado;
 
-public dusuario (){
-
-}
+public dusuario (){}
 
     public dusuario(int idusuario, String usuario, String contraseña, String cargos, String personal, String direccion, String telefono,  String estado) {
         this.idusuario = idusuario;
@@ -31,78 +22,40 @@ public dusuario (){
         this.personal = personal;
         this.direccion = direccion;
         this.telefono = telefono;
-    
         this.estado = estado;
     }
 
-    public int getIdusuario() {
-        return idusuario;
-    }
+    public int getIdusuario() { return idusuario; }
 
-    public void setIdusuario(int idusuario) {
-        this.idusuario = idusuario;
-    }
+    public void setIdusuario(int idusuario) { this.idusuario = idusuario;}
 
-    public String getUsuario() {
-        return usuario;
-    }
+    public String getUsuario() { return usuario; }
 
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
+    public void setUsuario(String usuario) {this.usuario = usuario; }
 
-    public String getContraseña() {
-        return contraseña;
-    }
+    public String getContraseña() { return contraseña; }
 
-    public void setContraseña(String contraseña) {
-        this.contraseña = contraseña;
-    }
+    public void setContraseña(String contraseña) { this.contraseña = contraseña; }
 
-    public String getCargos() {
-        return cargos;
-    }
+    public String getCargos() {return cargos; }
 
-    public void setCargos(String cargos) {
-        this.cargos = cargos;
-    }
+    public void setCargos(String cargos) {this.cargos = cargos; }
 
-    public String getPersonal() {
-        return personal;
-    }
+    public String getPersonal() {return personal;}
 
-    public void setPersonal(String personal) {
-        this.personal = personal;
-    }
+    public void setPersonal(String personal) {this.personal = personal;}
 
-    public String getDireccion() {
-        return direccion;
-    }
+    public String getDireccion() {return direccion;}
 
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
+    public void setDireccion(String direccion) {this.direccion = direccion;}
 
-    public String getTelefono() {
-        return telefono;
-    }
+    public String getTelefono() {return telefono;}
 
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
+    public void setTelefono(String telefono) { this.telefono = telefono;}
 
-   
+    public String getEstado() { return estado; }
 
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-
-
+    public void setEstado(String estado) {this.estado = estado; }
 
 
 }
