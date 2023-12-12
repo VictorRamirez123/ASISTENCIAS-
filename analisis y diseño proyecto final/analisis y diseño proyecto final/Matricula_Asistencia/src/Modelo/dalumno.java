c8f188a7dcbfcd69dd86a76b002c2e2199aed720
Alex123Palacin <152417813+Alex123Palacin@users.noreply.github.com>
2023-12-12 12:45:19 -0500

    Update dalumno.java

--- analisis y diseño proyecto final/analisis y diseño proyecto final/Matricula_Asistencia/src/Modelo/dalumno.java
+++ analisis y diseño proyecto final/analisis y diseño proyecto final/Matricula_Asistencia/src/Modelo/dalumno.java
@@ -1,31 +1,21 @@
 
 package Modelo;
 
-
 public class dalumno{
- 
 private int idcliente;
 private  String cedula;
 private  String nombres;
-
 private  String apellidos;
 private String fecha;
 private String lugar;
 private String madre;
 private String padre;
-
 private String direccion;
-
-
 private String documento;
 
-
-public dalumno (){
-
-}
+public dalumno (){}
 
 public dalumno(int idcliente,String cedula, String nombres,String apellidos,String fecha,String Direccion,String madre,String padre,String lugar,String  documento)  {
-
 this.idcliente= idcliente;
 this.cedula=cedula;
 this.nombres = nombres;
@@ -39,173 +29,46 @@ this.documento=documento;
 
 }
 
-//////////-******** primer campos ------------------------------
+public int  getid_codigo ()  {return idcliente;}
 
-public int  getid_codigo ()  {
+public void setid_codigo(int idcliente)  { this.idcliente = idcliente;}
 
-    return idcliente;
-    
-    
-}
+public String  getcedula()  { return cedula;}
 
+public void setcedula(String cedula)  {this.cedula=cedula;}
 
-public void setid_codigo(int idcliente)  {
+public String  getnombres()  { return nombres;}
 
+public void setnombres(String nombres)  { this.nombres =nombres;}
 
-  this.idcliente = idcliente;
+public String  getapellidos ()  { return apellidos;}
 
-    }
+public void setapellidos(String apellidos)  { this.apellidos = apellidos;}
 
 
-public String  getcedula()  {
+public String  getfecha()  {return fecha;}
 
-    return cedula;
-    
-    
-}
+public void setfecha(String fecha)  {this.fecha= fecha;}
 
-public void setcedula(String cedula)  {
+public String  getdireccion ()  {return direccion;}
 
+public void setdireccion(String direccion)  {this.direccion = direccion;}
 
-  this.cedula=cedula;
+public String  getlugar ()  { return lugar;}
 
-    
+public void setlugar(String lugar)  {this.lugar= lugar; }
 
+public String  getmadre ()  { return madre;}
 
-}
+public void setmadre(String madre)  { this.madre= madre;}
 
-/////////-*******************segundo campos ***********************************
+public String  getpadre ()  {return padre;}
 
-public String  getnombres()  {
+public void setpadre(String padre)  {this.padre= padre; }
 
-    return nombres;
-    
-    
-}
+public String  getdocumento ()  { return documento;}
 
-public void setnombres(String nombres)  {
-
-
-  this.nombres =nombres;
-
-    
-
-
-}
-
-/////////-******************* tercero campos ***********************************
-
-public String  getapellidos ()  {
-
-    return apellidos;
-    
-    
-}
-
-public void setapellidos(String apellidos)  {
-
-
-  this.apellidos = apellidos;
-
-    
-}
-
-
-public String  getfecha()  {
-
-    return fecha;
-    
-    
-}
-
-public void setfecha(String fecha)  {
-
-
-  this.fecha= fecha;
-
-    }
-
-/////////-*******************cuarto  campos ***********************************
-
-public String  getdireccion ()  {
-
-    return direccion;
-    
-    
-}
-
-public void setdireccion(String direccion)  {
-
-
-  this.direccion = direccion;
-
-    
-
-
-}
-
-/////////-*******************quinto campos ***********************************
-
-public String  getlugar ()  {
-
-    return lugar;
-    
-    
-}
-
-public void setlugar(String lugar)  {
-
-
-  this.lugar= lugar;
-
-    }
-
-
-/////////-*******************sexto  campos ***********************************
-
-public String  getmadre ()  {
-
-    return madre;
-    
-    
-}
-
-public void setmadre(String madre)  {
-
-
-  this.madre= madre;
-
-    }
-
-public String  getpadre ()  {
-
-    return padre;
-    
-    
-}
-
-public void setpadre(String padre)  {
-
-
-  this.padre= padre;
-
-    }
-
-
-
-public String  getdocumento ()  {
-
-    return documento;
-    
-    
-}
-
-public void setdocumento(String documento)  {
-
-
-  this.documento=documento;
-
-    }
+public void setdocumento(String documento)  {this.documento=documento;}
 
 
 
