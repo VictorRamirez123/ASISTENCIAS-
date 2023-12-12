234d59fbcf7e8dc9edf63266b340b140e9643cc8
GrayPadilla123 <150388680+GrayPadilla123@users.noreply.github.com>
2023-12-12 14:58:31 -0500

    Update bd_matricula_asistencia.sql

--- analisis y diseño proyecto final/analisis y diseño proyecto final/Base Dato/bd_matricula_asistencia.sql
+++ analisis y diseño proyecto final/analisis y diseño proyecto final/Base Dato/bd_matricula_asistencia.sql
@@ -19,3 +19,84 @@ CREATE TABLE `asistencia` (
 INSERT INTO `estudiante` (`idEstudiante`, `identificacion`, `Nombres`, `Apellidos`, `fecha_nacimiento`, `Lugar_de_nacimiento`, `Madre`, `Padre`, `Domicilio`, `Documento_Presentados`) VALUES
 (1, '98745879', 'luis', 'perales roca', '9/12/2015', 'lima', 'ana roca vicente', 'saul perales lomas', 'av sauce 14 lima', 'certificado estudio disponible');
 
+CREATE TABLE `grado` (
+  `idgrado` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `nivel` varchar(45) DEFAULT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
+) ENGINE=InnoBD DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `grado` (`idgrado`, `nombre`, `nivel`, `descripcion`) VALUES
+(1, 'primero', 'primaria', 'instruccion grado'),
+(2, 'segundo', 'primaria', 'instruccion grado');
+
+CREATE TABLE `matricula` (
+  `idmatricula` int(11) NOT NULL,
+  `idestudiante` int(11) DEFAULT NULL,
+  `idgrado` int(11) DEFAULT NULL,
+  `periodo` varchar(45) DEFAULT NULL,
+  `estado` varchae(45) DEFAULT NULL
+) ENGINE=InnoBD DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `matricula` (`idmatricula`, `idestudiante`, `idgrado`, `periodo`, `estado`) VALUES
+(1, 1, 1, '2023', 'Activo');
+
+CREATE TABLE `usuario` (
+  `cod_usuario` int(11) NOT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `contraseña` varchar(45) DEFAULT NULL,
+  `cargo` varchar(45) DEFAULT NULL,
+  `persona` varchar(45) DEFAULT NULL,
+  `direccion` varchar(45) DEFAULT NULL,
+  `telefono` varchar(45) DEFAULT NULL,
+  `estado` varchar(45) DEFAULT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `usuario` (`cod_usuario`, `usuario`, `contraseña`, `cargo`, `persona`, `direccion`, `telefono`, `estado`) VALUES
+(1, 'admin', '123', 'Administrador', 'luis perales roca', 'av lince 17', '3256478', 'disponible');
+(2, 'luis', '12', 'Supervisor', 'luis', 'av sauce 14', '6547874', 'disponible');
+
+ALTER TABLE `asistencia`
+  ADD PRIMARY KEY (`idasistencia`),
+  ADD KEY `idestudiante` (`idestudiante`),
+  ADD KEY `idgrado` (`idgrado`),
+  ADD KEY `cod_usuario` (`cod_usuario`);
+
+ALTER TABLE `estudiante`
+  ADD PRIMARY KEY (`idEstudiante`);
+
+ALTER TABLE `grado`
+  ADD PRIMARY KEY (`idgrado`);
+
+ALTER TABLE `matricula`
+  ADD PRIMARY KEY (`idmatricula`),
+  ADD KEY `idestudiante` (`idestudiante`),
+  ADD KEY `idgrado` (`idgrado`);
+
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`cod_usuario`);
+
+ALTER TABLE `asistencia`
+  MODIFY `idasistencia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+ALTER TABLE `estudiante`
+  MODIFY `idEstudiante` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+ALTER TABLE `grado`
+  MODIFY `igrado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+ALTER TABLE `matricula`
+  MODIFY `idmatricula` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+ALTER TABLE `usuario`
+  MODIFY `cod_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+ALTER TABLE `asistencia`
+  ADD CONSTRAINT `asistencia_ibfk_1` FOREIGN KEY (`idestudiante`) REFERENCES `estudiante` (`idEstudiante`),
+  ADD CONSTRAINT `asistencia_ibfk_2` FOREIGN KEY (`idgrado`) REFERENCES `grado` (`idgrado`),
+  ADD CONSTRAINT `asistencia_ibfk_3` FOREIGN KEY (`cod_usuario`) REFERENCES `usuario` (`cod_usuario`);
+
+ALTER TABLE `matricula`
+  ADD CONSTRAINT `matricula_ibfk_1` FOREIGN KEY (`idestudiante`) REFERENCES `estudiante` (`idEstudiante`),
+  ADD CONSTRAINT `matricula_ibfk_2` FOREIGN KEY (`idgrado`) REFERENCES `grado` (`idgrado`);
+COMMIT;
