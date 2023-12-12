7cafbb584a57e9297461c11ab87f6a7932f572ac
Alex123Palacin <152417813+Alex123Palacin@users.noreply.github.com>
2023-12-12 12:51:26 -0500

    Update conexion.java

--- analisis y diseño proyecto final/analisis y diseño proyecto final/Matricula_Asistencia/src/Controladores/conexion.java
+++ analisis y diseño proyecto final/analisis y diseño proyecto final/Matricula_Asistencia/src/Controladores/conexion.java
@@ -1,49 +1,33 @@
-
-
 package Controladores;
 
-import java. sql.Connection;
-
-import java. sql.DriverManager;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import javax.swing.JOptionPane;
 
+public class conexion {
 
-public class conexion{
-    
-    
-    public String db="BD_Matricula_Asistencia";
-    
-    public String  url="jdbc:mysql://localhost/" +   db;
-    
-    public  String  user="root";
-    public  String pass="";
-    
-    
-    public  Connection Connectar()
-    {
-        
-       Connection link=null;
-       try
-           {
-               
-               Class.forName("org.gjt.mm.mysql.Driver");
-               link=DriverManager.getConnection(url,user,pass);
-               
-               
-               
-                }  catch(Exception e)   {
-                    
-                    
-                    JOptionPane.showMessageDialog(null,e);
-               
-               }
-               
-               return  link;
-               } 
-               
-               
-        
-    
-    
-    
+    public String db = "BD_Matricula_Asistencia";
+    public String url = "jdbc:mysql://localhost/" + db;
+    public String user = "root";
+    public String pass = "";
+
+    public Connection Connectar() {
+
+        Connection link = null;
+        try {
+
+            Class.forName("org.gjt.mm.mysql.Driver");
+            link = DriverManager.getConnection(url, user, pass);
+
+        } catch (Exception e) {
+
+            JOptionPane.showMessageDialog(null, e);
+
+        }
+
+        return link;
+    }
+
 }
+
+
